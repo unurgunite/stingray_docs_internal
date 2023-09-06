9924d2059cae67449ded7b105e0be95b22eedda6
Alex Abramov <omgout.200@gmail.com>
2023-09-06 18:01:44 +0700

    fixed requires for `stingray_docs_internal`

--- lib/stingray_docs_internal.rb
+++ lib/stingray_docs_internal.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "stingray_docs_internal/version"
-
 module StingrayDocsInternal
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require_relative "stingray_docs_internal/version"
+require_relative "extensions/extensions"
--- lib/stingray_docs_internal/generator.rb
+++ lib/stingray_docs_internal/generator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "../stingray_docs_internal"
+
 module StingrayDocsInternal # :nodoc:
   module Generator # :nodoc:
     class << self
