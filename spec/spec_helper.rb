1144bcde1270d74e2e5ae46281a6c3b4881d9990
[NSObject init] <49247588+unurgunite@users.noreply.github.com>
2026-09-11 00:36:37 +0300

    [1.6.2] Fix config command and status check (#96)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,7 @@ RSpec.configure do |config|
   config.include CollectionLoaderHelper
   config.include KeepDescriptionsHelper
   config.include NormalizeCommentHelper
+  config.include DummyServerHelper
   config.example_status_persistence_file_path = '.rspec_status'
   config.disable_monkey_patching!
   config.expect_with(:rspec) { |c| c.syntax = :expect }
